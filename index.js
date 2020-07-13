7fbf9bbc949083c6df7fb236550edebcb754500e
vladimir talas <vladimir.talas@gmail.com>
2020-07-13 21:42:00 +0200

    temp

--- index.js
+++ index.js
@@ -13,9 +13,4 @@ var mySwiper = new Swiper('.swiper-container', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-
-	// And if we need scrollbar
-	scrollbar: {
-		// el: '.swiper-scrollbar',
-	},
 })
